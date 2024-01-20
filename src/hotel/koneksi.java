106632f512c07a031b571ff1391628e7afc3548e
Ariyuka <23083000057@student.unmer.ac.id>
2024-01-20 09:53:55 +0700

    first commit

---
+++ src/hotel/koneksi.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package hotel;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Vivobook
+ */
+public class koneksi {
+    Connection Koneksi=null;
+
+    public static Connection koneksiDb() 
+    {
+        try{
+            String url="jdbc:mysql://localhost/hotel"; // nama database
+            String user="root";
+            String pass="";
+            Class.forName("com.mysql.jdbc.Driver");     // nama driver MySQL
+            Connection koneksi=DriverManager.getConnection(url,user,pass);  // membuat koneksi
+            return koneksi;
+        }
+        catch(Exception e)
+        {
+            JOptionPane.showMessageDialog(null, e); // menampilkan perintah jika koneksi gagal
+            return null;
+        }
+    
+    }
+    
+}
